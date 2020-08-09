4d6da41565ba3ebfd22d1d895f7d7a19f5735072
Mona Lisa <53441584+ajyoung21@users.noreply.github.com>
2020-08-09 16:20:41 -0400

    Added Top Shotta

--- static/js/Descriptions/E_Desc.js
+++ static/js/Descriptions/E_Desc.js
@@ -1,4 +1,5 @@
 d3.select("#changes").on("click", changes)
+d3.select("#nle").on("click", choppa)
 d3.select("#E_Desc").on("click", close)
 
 
@@ -55,6 +56,50 @@ Mr. Bieber.</p>
 <li>Anything but 'Intentions'</li>
 <li>Anything but 'Yummy'</li>
 <li>The Clever feature was kinda nice.</li>
+</ol></p>
+
+   </p><button id="Close">CLOSE</button>`
+)
+
+}
+
+function choppa(){
+
+    close()
+    var info = d3.select("#E_Desc")
+
+console.log('NLE Choppa - Top Shotta')
+
+info.html(`<p>NLE Choppa</p>
+<p>Top Shotta</p>
+
+<p>I'm a believer in NLE Choppa. The kid (he's 17) can rap and from what I've seen, he's a 
+nice guy that loves his mom. I personally find him easy to root for.</p>
+
+<p>That said, he still has a LONG way to go as an artist. When NLE is in his wheelhouse 
+(as he is on the "Shotta Flow 3," "Top Shotta Flow," "Shotta Flow 4," "Shotta Flow 5" section of the album
+     - yes that is a 4-song stretch), he can make some hits.
+</p>
+
+<p>When Top Shotta strays from the proven formula, the results are painful. "Make Em Say," a crude sexual song
+is just cringey and one of the five worst songs that I've heard this year. The more low-key, introspective tracks
+are bland and boring, even if his bars about depression are coming from a genuine place.
+</p>
+
+<p>I can't knock NLE Choppa for trying to be more versatile, even if the inclusion of these tracks feels like 
+checking a box for the label. He's 17, of course there going to be some cringe on this tape. While I never expect him
+to develop into a well-rounded artist, I expect the hits to keep coming, even if it's just "Shotta Flow
+26."
+</p>
+
+
+
+
+<p>Favorite Tracks: 
+<ol>
+<li>Camelot</li>
+<li>Who TF Up In My Trap</li>
+<li>Shotta Flow 4</li>
 </ol></p>
 
    </p><button id="Close">CLOSE</button>`
